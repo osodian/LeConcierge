784f2b947abbca67969885752bb6d3801fcb4d8f
Oscar Sodian <oscar.sodian@gmx.de>
2021-08-25 10:55:39 +0200

    morning

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :trips do
-    resources :activity_bookings, only: [:new, :create, :update, :edit, :destroy]
-    resources :hotel_bookings, only: [:new, :create, :update, :edit, :destroy]
+    resources :activity_bookings, only: [:new, :create, :update, :edit, :destroy, :show ]
+    resources :hotel_bookings, only: [:new, :create, :update, :edit, :destroy, :show ]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
