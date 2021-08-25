b1c6266711ce576d489dd78e65879caaa0ce4172
Louis Labes <labeslouis@gmail.com>
2021-08-25 15:01:22 +0200

    MAP

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,6 @@
 class Activity < ApplicationRecord
-    has_many :activity_bookings
+  has_many :activity_bookings
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
