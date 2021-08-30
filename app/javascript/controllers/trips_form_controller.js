c180696e29e1dfe7a45b908c4bbb8e1f31065c5f
Friedrich Zoellner <f.s.zoellner@gmail.com>
2021-08-30 09:01:00 +0200

    hs

--- app/javascript/controllers/trips_form_controller.js
+++ app/javascript/controllers/trips_form_controller.js
@@ -2,8 +2,14 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 
-  static targets = ["timeframe", "people", "transport", "submit", "group", "timeframeTab", "peopleTab", "transportTab"]
+  static targets = ["destination", "timeframe", "people", "transport", "submit", "group", "destinationTab", "timeframeTab", "peopleTab", "transportTab"]
 
+
+  revealDestination() {
+    this.destinationTabTarget.classList.add('completed')
+    this.groupTargets.forEach(group => group.classList.add('d-none'));
+    this.destinationTarget.classList.remove("d-none")
+  }
 
   revealTimeframe() {
     this.timeframeTabTarget.classList.add('completed')
@@ -28,4 +34,38 @@ export default class extends Controller {
     this.submitTarget.classList.remove("d-none")
   }
 
+  previousDestination() {
+    this.timeframeTabTarget.classList.remove('completed')
+    this.peopleTabTarget.classList.remove('completed')
+    this.transportTabTarget.classList.remove('completed')
+    this.destinationTabTarget.classList.add('completed')
+    this.groupTargets.forEach(group => group.classList.add('d-none'));
+    this.destinationTarget.classList.remove("d-none")
+  }
+
+  previousTimeframe() {
+    this.transportTabTarget.classList.remove('completed')
+    this.peopleTabTarget.classList.remove('completed')
+    this.timeframeTabTarget.classList.add('completed')
+    this.groupTargets.forEach(group => group.classList.add('d-none'));
+    this.timeframeTarget.classList.remove("d-none")
+  }
+
+  previousPeople() {
+    this.transportTabTarget.classList.remove('completed')
+    this.peopleTabTarget.classList.add('completed')
+    this.groupTargets.forEach(group => group.classList.add('d-none'));
+    this.peopleTarget.classList.remove("d-none")
+  }
+
+  previousTransport() {
+    this.transportTabTarget.classList.add('completed')
+    this.groupTargets.forEach(group => group.classList.add('d-none'));
+    this.transportTarget.classList.remove("d-none")
+  }
+
+  previousSubmit() {
+    this.groupTargets.forEach(group => group.classList.add('d-none'));
+    this.submitTarget.classList.remove("d-none")
+  }
 }
